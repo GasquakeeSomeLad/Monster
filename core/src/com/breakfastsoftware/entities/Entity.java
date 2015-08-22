9e70fd90e9ace82bb6d2ea2454dbf05ac0054438
GasquakeeSomeLad <GasquakeeSomeLad@gmail.com>
2015-08-21 14:28:18 -1100

    Stuff

---
+++ core/src/com/breakfastsoftware/entities/Entity.java
@@ -0,0 +1,7 @@
+package com.breakfastsoftware.entities;
+
+/**
+ * Created by SomeLad on 8/21/2015.
+ */
+public class Entity {
+}
---
+++ core/src/com/breakfastsoftware/states/State.java
@@ -0,0 +1,8 @@
+package com.breakfastsoftware.states;
+
+/**
+ * Created by SomeLad on 8/21/2015.
+ */
+public class State {
+
+}
